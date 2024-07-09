c4941e5a961f56fc622565a7ad834c01413fe349
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-09 10:11:07 -0300

    aula

---
+++ pythonteste/aula06a.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+# print('A soma entre', n1, 'e', n2, 'vale', s)
+print('A soma entr {} e {} vale {}'.format(n1, n2, s))
+
+
+
+
