1fbbbd5096941fb70c402f8ee6822e0cc9a1cdef
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-09 10:40:34 -0300

    Update ex004.py

---
+++ Exercicios/ex004.py
@@ -0,0 +1,11 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfabético? ', a.isalnum())
+print('Está em maiúsculasa? ', a.isupper())
+print('Está em minúsculas? ', a.islower())
+print('Está capitalizadaa? ', a.istitle())
+
+
