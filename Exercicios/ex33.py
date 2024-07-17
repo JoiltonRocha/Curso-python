b0afbe6735910a9c6bcd29709ed97a62fa08e36f
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-17 11:13:17 -0300

    Update ex33.py

--- Exercicios/ex33.py
+++ Exercicios/ex33.py
@@ -3,4 +3,17 @@ print('====== DESAFIO 33 ======')
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-if
+#Verificando quem é menor
+menor = n1
+if n2 < n1 and n3 < n2:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+#Verificando quem é maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print(f'O menor número digitado foi {menor}.')
+print(f'O maior número digitado foi {maior}.')
