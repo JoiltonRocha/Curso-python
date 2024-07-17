724b3622e84a72431fd600387053383271fc6d70
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-17 10:08:04 -0300

    Create ex33.py

---
+++ Exercicios/ex33.py
@@ -0,0 +1,6 @@
+#Ex33: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+print('====== DESAFIO 33 ======')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+if 
