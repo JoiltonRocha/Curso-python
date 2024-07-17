4b4f11ef08d605240b12590f73731f676a178b65
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-17 10:46:00 -0300

    Update ex029.py

--- Exercicios/ex029.py
+++ Exercicios/ex029.py
@@ -1,10 +1,11 @@
 #Ex029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, ...
 #...mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
 print('====== DESAFIO 29 ======')
-v = int(input(f'Digite a velocidade em Km/h: '))
+v = float(input(f'Digite a velocidade em Km/h: '))
 if v > 80:
     ex = v - 80
-    vlr = float(ex * 7)
-    print(f'Você ultrapassou em {ex}Km/h a velocidade máxima permitida e foi multado em R${vlr:.2f}')
+    vlr = (ex * 7)
+    print(f'MULTADO! Você ultrapassou em {ex}Km/h a velocidade máxima permitida que é de 80Km/h e foi multado em R${vlr:.2f}')
+    print('Dirija com segurança!')
 else:
-    print('')
+    print('Mantenha-se dentro da velocidade de segurança!')
