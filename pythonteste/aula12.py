1254fe96e5adc0bfe95ded668fc12cc07630c0f8
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-17 20:04:09 -0300

    Create aula12.py

---
+++ pythonteste/aula12.py
@@ -0,0 +1,10 @@
+nome = str(input('Qual é seu nome? '))
+if nome == 'Joilton':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or 'Maria' or 'Paulo':
+    print('Seu nom é bem popular no Brasil.')
+elif: nome == in ('Ana Claudia Jéssica Juliana')
+    print('Belo nom feminino!')
+else:
+    print('Seu nome é bem normal.')
+print(f'Tenha um bom dia, {nome}!')
