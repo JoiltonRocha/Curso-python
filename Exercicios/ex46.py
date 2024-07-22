3cc8d98fc9e0629a323a27548b03ec4b6d6ec1df
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-22 17:57:32 -0300

    Create ex46.py

---
+++ Exercicios/ex46.py
@@ -0,0 +1,7 @@
+'''Ex46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos
+de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
+from time import sleep
+for c in range(10, 0, -1):
+    sleep(1)
+    print(c)
+print('BUUUUUUM!!!')
