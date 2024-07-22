b672c273b38ca70e95d4cf0f4474ca5579f17a57
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-22 19:11:00 -0300

    aula

---
+++ Exercicios/ex049.py
@@ -0,0 +1,4 @@
+'''Ex49: Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que
+agora utilizando um laço for.'''
+print(f'{'DESAFIO 49':=^45}')
+n = int(input('Digite um número: '))
