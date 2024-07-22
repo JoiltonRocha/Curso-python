d862f13a69fc6bf0a19c0605386a51ea160deebc
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-22 09:15:47 -0300

    Update ex42.py

--- Exercicios/ex42.py
+++ Exercicios/ex42.py
@@ -14,3 +14,18 @@ elif r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2 and r1 != r2 and r1 != r3 an
     print('Os segmentos acima formam um triângulo ESCALENO.')
 else:
     print('Os segmentos acima não podem formar triângulo!')
+
+'''OUTRA FORMA
+r1 = float(input('Digite o primeiro segmento: '))
+r2 = float(input('Digite o segundo segmento: '))
+r3 = float(input('Digite o terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima Formam um triângulo ', end='')
+    if r1 == r2 == r2:
+        print('EQUILÁTERO!')
+    elif r1 == r2 or r1 == r3 or r2 == r3:
+        print('ISÓSCELES!')
+    else:
+        print('ESCALENO!')
+else:
+    print('Os segmentos acima não podem formar triângulo!')'''
