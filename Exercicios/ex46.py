94e9b212d4412ed5ad5f7a38c50221319c2b6b53
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-22 18:04:43 -0300

    Update ex46.py

--- Exercicios/ex46.py
+++ Exercicios/ex46.py
@@ -1,5 +1,6 @@
 '''Ex46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos
 de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
+print(f'{'DESAFIO 46':=^45}')
 from time import sleep
 for c in range(10, 0, -1):
     sleep(1)
