8234504f173188180127b92a95f9c2a19e951b15
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-22 19:06:26 -0300

    Create ex48.py

---
+++ Exercicios/ex48.py
@@ -0,0 +1,8 @@
+'''Ex48: Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos
+de três e que se encontram no intervalo entre 1 e 500.'''
+print(f'{'DESAFIO 48':=^45}')
+for c in range(3, 500, 3):
+    if c % 2 != 0:
+        print(c)
+    else:
+        print(end='')
