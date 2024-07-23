28a26c4fe75ce772d3951c7cc537ce172c5f3973
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 10:47:55 -0300

    Update ex051.py

--- Exercicios/ex051.py
+++ Exercicios/ex051.py
@@ -1,3 +1,9 @@
 '''Ex51: Desenvolva um programa que leia o primeiro número e a razão de uma PA. No final,
 mostre os 10 primeiros termos dessa progressão.'''
 print(f'{'DESAFIO 51':=^45}')
+primeirotermo = int(input('Digite o primeiro termo:'))
+razão = int(input('Digite a razão: '))
+décimotermo = primeirotermo + (10 - 1) * razão
+for c in range(primeirotermo, décimotermo + razão, razão):
+    print(f'{c}', end=' -> ')
+print('Acabou')
