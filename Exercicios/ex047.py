425a9c710189d49c69a1659525e8b3c14ac9c9a3
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 09:35:00 -0300

    Update ex047.py

--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -1,5 +1,5 @@
 '''Ex47: Cre um programa que mostre na tela todos os números pares que estão no intervalo
 entre 1 e 50.'''
 print(f'{'DESAFIO 47':=^45}')
-for c in range(0, 50+1, 2):
-    print(c)
+for c in range(2, 50+1, 2):
+    print(c, end=' ')
