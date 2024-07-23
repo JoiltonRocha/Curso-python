1bc23fa91952f50aaf14f0860ebbfd6adbf2147e
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 08:51:30 -0300

    Update ex048.py

--- Exercicios/ex048.py
+++ Exercicios/ex048.py
@@ -1,8 +1,9 @@
 '''Ex48: Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos
 de três e que se encontram no intervalo entre 1 e 500.'''
 print(f'{'DESAFIO 48':=^45}')
+s = 0
 for c in range(3, 500, 3):
     if c % 2 != 0:
+        s += c
         print(c)
-    else:
-        print(end='')
+
