38c308534d351fd3b8778759ebc1b3ffaa1747be
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 10:30:33 -0300

    Update ex050.py

--- Exercicios/ex050.py
+++ Exercicios/ex050.py
@@ -1,10 +1,12 @@
 '''Ex50: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles
-que forem pares. Se o valor degitado for ímpar, desconsidere-o.'''
+que forem pares. Se o valor digitado for ímpar, desconsidere-o.'''
 print(f'{'DESAFIO 50':=^45}')
-s = 0
-for c in range(0, 6):
-    n = int(input('Digite um número:'))
-    if n % 2 == 0:
-        s += n
-print(f'A soma dos números pares digitados é {s}')
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input(f'Digite o {c}º número:'))
+    if num % 2 == 0:
+        soma = soma + num # Pode ser escrito assim: soma += num
+        cont = cont + 1 # Pode ser escrito assim: cont += 1
+print(f'Foram digitados {c} números pares e a soma deles é {soma}.')
 
