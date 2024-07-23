37f44d3cf3f06ce76b4aa2dae3de3e31aeb2de9b
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 16:40:43 -0300

    Update ex054.py

--- Exercicios/ex054.py
+++ Exercicios/ex054.py
@@ -1,3 +1,15 @@
-'''Ex54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas
-pessoas ainda não atingiram a maioridade e quantas já são maiores.'''
+'''Ex54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre
+quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.'''
 print(f'{'DESAFIO 54':=^45}')
+from datetime import date
+anoatual = date.today().year
+totmaior = 0
+totmenor = 0
+for c in range(1, 8):
+    anonasc = int(input(f'Digite o ano de nascimento da {c}ª pessoa: '))
+    idade = anoatual - anonasc
+    if idade >= 21:
+        totmaior = totmaior + 1 #Pode ser escrito: totmaior += 1
+    else:
+        totmenor = totmenor + 1 #Pode ser escrito: totmenor += 1
+print(f'Total de pessoas maiores de idade: {totmaior}.\nTotal de pessoas menores de idade: {totmenor}.')
