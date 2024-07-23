606e57ee184b46a7b25d7efe1ffb39c624352a50
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 11:14:27 -0300

    Update ex054.py

--- Exercicios/ex054.py
+++ Exercicios/ex054.py
@@ -1,2 +1,3 @@
 '''Ex54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas
 pessoas ainda não atingiram a maioridade e quantas já são maiores.'''
+print(f'{'DESAFIO 54':=^45}')
