5391d87f3aeae4d77ad4321472317e337f4d38e6
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-23 10:14:46 -0300

    Update ex049.py

--- Exercicios/ex049.py
+++ Exercicios/ex049.py
@@ -1,7 +1,6 @@
 '''Ex49: Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que
 agora utilizando um laço for.'''
 print(f'{'DESAFIO 49':=^45}')
-n = int(input('Digite um número: '))
-for c in range(1, 9+1):
-    t = n * c
-    print(t)
+n = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 11):
+    print(f'{n} x {c:2} = {n * c:2}')
