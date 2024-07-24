50f24eef617e7449f42de4577a24f4848185f9bf
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-24 19:53:10 -0300

    Update ex058.py

--- Exercicios/ex058.py
+++ Exercicios/ex058.py
@@ -2,22 +2,25 @@
 Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram
 nescessários para vencer.'''
 print(f'{'DESAFIO 58':=^45}')
-import random
+from random import randint
 from time import sleep
 print('Vamos jogar? Escolha um intervalo numérico a partir de 2 dígitos (quanto maior o intervalo, maior a dificuldade):')
-computador = jogador = 0
-c = 1
-while computador == jogador:
-    print('PARABÉNS! Você acertou!')
-    print('Não foi desssa vez. Continue tentando.')
-    n1 = int(input('Número inicial: '))
-    n2 = int(input('Número final: '))
-    computador = random.randint(n1, n2) #Faz o computador "pensar"
-    print(f'Vou pensar em um número entre {n1} e {n2}. Tente adivinhar...')
-    jogador = int(input('Qual número você pensou? ')) #Jogador tenta adivinhar
+n1 = int(input('Número inicial: '))
+n2 = int(input('Número final: '))
+computador = randint(n1, n2)
+print(f'Ok! Vamos começar... vou pensar em um número entre {n1} e {n2}. Tente adivinhar...')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual número você acha que eu pensei? '))
     print('PROCESSANDO...')
-    sleep(2)
+    sleep(0.5)
+    palpites = palpites + 1 # Pode ser escrito: palpites += 1
     if jogador == computador:
-    print('PARABÉNS! Você acertou!')
+        acertou = True
     else:
-    print(f'GANHEI! Eu pensei no número {computador} e não no {jogador}')
+        if jogador < computador:
+            print('Pensei em um número maior... Tente novamente.')
+        else:
+            print('Pensei em um número menor... Tente novamente.')
+print(f'Acertou com {palpites} tentativas.')
