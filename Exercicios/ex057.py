d766199f94ad2cc168c5843fb26e72c6f99bcc61
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-24 18:51:51 -0300

    Update ex057.py

--- Exercicios/ex057.py
+++ Exercicios/ex057.py
@@ -1,11 +1,7 @@
-'''Ex57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
-Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
+'''Ex57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores
+'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
 print(f'{'DESAFIO 57':=^45}')
-sexo = 1
-while sexo != 0:
-    sexo = str(input('Digite o sexo [M/F]:')).upper()
-    if sexo = M or F:
-        print('Aceito!')
-    else:
-        print('Opção inválida. Escolha uma das opções.')
-print('FIM')
+sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados inválidos. Por favor informe seu sexo [M/F]:')).upper().strip()[0]
+print(f'Sexo {sexo} registrado com sucesso!')
