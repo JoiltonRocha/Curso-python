3131e5c760e9d0e9bf4fbd6e8dacc2b35bf919a3
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-26 09:57:35 -0300

    Update aula14.py

--- pythonteste/aula14.py
+++ pythonteste/aula14.py
@@ -2,9 +2,9 @@ n = 1
 par = impar = 0
 while n !=0:
     n = int(input('Digite um número: '))
-    if n != 0:
+    if n != 0: #Para não contar o número '0'.
         if n % 2 == 0:
-            par = par + 1 #Pode ser escrito: par += 1
+            par = par + 1 #Pode ser escrito: par += 1.
         else:
             impar = impar + 1
 print(f'Você digitou {par} números pares e {impar} números ímpares.')
