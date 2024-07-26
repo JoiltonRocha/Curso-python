f1168800d1fa5fdf13b7eee39cb6bd0ca8fc43c4
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-26 13:09:11 -0300

    Create ex063.py

---
+++ Exercicios/ex063.py
@@ -0,0 +1,4 @@
+'''Ex63: Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n
+primeiros elementos de  mma Sequência de Fibonacci.
+Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8.'''
+print(f'{'DESAFIO 63':=^45}')
