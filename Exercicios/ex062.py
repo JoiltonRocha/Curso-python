b9121f315719f218149567e4830eb01dc4c7e69a
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-26 13:05:19 -0300

    Create ex062.py

---
+++ Exercicios/ex062.py
@@ -0,0 +1,3 @@
+'''Ex62: Melhore o desafio 61, perguntando para o usuário se ele quer mostrar mais alguns
+termos. O programa encerra quando ele disser que quer mostrar 0 termos.'''
+print(f'{'DESAFIO 62':=^45}')
