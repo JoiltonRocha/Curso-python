a77c8cafd9fe1f783b2a53a93141b40d85396cfc
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-26 13:01:00 -0300

    Create ex061.py

---
+++ Exercicios/ex061.py
@@ -0,0 +1,3 @@
+'''Ex61: Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
+primeiros termos da progressão usando a estrutura while.'''
+print(f'{'DESAFIO 61':=^45}')
