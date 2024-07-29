3e5875342cae7ffb0bead34bce32de13dac3dea2
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-29 08:44:37 -0300

    Update ex061.py

--- Exercicios/ex061.py
+++ Exercicios/ex061.py
@@ -1,3 +1,12 @@
 '''Ex61: Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
 primeiros termos da progressão usando a estrutura while.'''
 print(f'{'DESAFIO 61':=^45}')
+primeirotermo = int(input('Digite o primeiro termo:'))
+razão = int(input('Digite a razão: '))
+termo = primeirotermo
+cont = 1
+while cont <= 10:
+    print(f'{termo} -> ', end='')
+    termo = termo + razão
+    cont = cont + 1
+print('Acabou')
