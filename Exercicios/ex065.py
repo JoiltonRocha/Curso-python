47ff9375b2bfe8c0748ef084ac0f0ecab099e096
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-29 08:06:21 -0300

    Create ex065.py

---
+++ Exercicios/ex065.py
@@ -0,0 +1,4 @@
+'''Ex65: Crie um programa que leia vários números inteiros pelo teclado. No final fa execução
+mostre a média entre todos os valores e qual foi o maior e o menor valor lido. O programa deve
+perguntar ao usuário se ele quer ou não continuar a digitar valores.'''
+print(f'{'DESAFIO 65':=^45}')
