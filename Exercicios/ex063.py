a88be21e82c69d5e41fd46333aa2245565bed003
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-29 09:26:21 -0300

    Update ex063.py

--- Exercicios/ex063.py
+++ Exercicios/ex063.py
@@ -2,3 +2,15 @@
 primeiros elementos de  mma Sequência de Fibonacci.
 Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8.'''
 print(f'{'DESAFIO 63':=^45}')
+num = int(input('Quantos termos você que mostrar? '))
+t1 = 0
+t2 = 1
+print(f'{t1} -> {t2}', end='')
+cont = 3
+while cont <= num:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont = cont + 1
+print(' -> FIM')
