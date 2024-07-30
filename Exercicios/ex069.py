79140cb6200042f30600227eb926cb466ca16391
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-30 09:15:23 -0300

    Update ex069.py

--- Exercicios/ex069.py
+++ Exercicios/ex069.py
@@ -6,6 +6,7 @@ C) Quantas mulheres tem menos de 20 anos.'''
 print(f'{'DESAFIO 69':=^45}')
 totp18 = totm = totf20 = 0
 while True:
+    print(f'{'CADASTRE UMA PESSOA':-^40}')
     idade = int(input('Digite a idade: '))
     sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
     if idade > 18:
@@ -16,17 +17,17 @@ while True:
         totf20 = totf20 + 1
     if sexo not in 'MF':
         while sexo not in 'MF':
-            print('Opção inválida.')
             sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
+    print('-' * 40)
     continuar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
     if continuar not in 'SN':
         while continuar not in 'SN':
-            print('Opção inválida.')
             continuar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    print('=' * 40)
     if continuar in 'N':
         break
 print(f'{'CADASTRO FINALIZADO!':*^65}')
-print(f'* Total de pessoas cadastradas com idade superior a 18 anos: {totp18}. *')
-print(f'* Total de homens cadastrados: {totm}.                               *')
-print(f'* Total de mulheres cadastradas com idade inferior a 20 anos: {totf20}.*')
+print(f'* Total de pessoas cadastradas com idade superior a 18 anos: {totp18}  *')
+print(f'* Total de homens cadastrados: {totm}                                *')
+print(f'* Total de mulheres cadastradas com idade inferior a 20 anos: {totf20} *')
 print('*' * 65)
