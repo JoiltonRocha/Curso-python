9a8fa309c6ff952eeb8d1d19da4d1fe5cd3be4ac
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-30 18:01:30 -0300

    Update ex071.py

--- Exercicios/ex071.py
+++ Exercicios/ex071.py
@@ -3,7 +3,8 @@ ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai i
 de cada valor serão entregues.
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
 print(f'{'DESAFIO 71':=^45}')
-cinquenta = vinte = dez = um = resto1 = resto50 = resto20 = resto10 = 0
+#COMO EU TENTEI FAZER:
+'''cinquenta = vinte = dez = um = resto1 = resto50 = resto20 = resto10 = 0
 valor = int(input('Digite o valor a ser sacado: R$ '))
 if valor % 50 == 0:
     cinquenta = valor / 50
@@ -29,9 +30,6 @@ if resto50 == 10:
     dez = resto50 / 10
 if resto50 < 10:
     um = resto50
-
-
-
 if cinquenta > 0:
     print(f'{cinquenta:.0f} notas de R$50,00')
 if vinte > 0:
@@ -39,4 +37,29 @@ if vinte > 0:
 if dez > 0:
     print(f'{dez:.0f} notas de R$10,00')
 if um > 0:
-    print(f'{um:.0f} notas de R$ 1,00')
+    print(f'{um:.0f} notas de R$ 1,00')'''
+
+
+
+#Bendito 'While'!
+valor = int(input('Digite o valor a ser sacado: R$ '))
+total = valor
+céd = 50
+totcéd = 0
+while True:
+    if total >= céd:
+        total = total - céd
+        totcéd = totcéd + 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+print('Essas cédulas serão entregues a seguir.')
