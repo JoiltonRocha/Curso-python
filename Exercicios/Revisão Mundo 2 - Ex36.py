9505b4a7d3c9403254100abbbf38e3eb49c72471
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-31 10:43:36 -0300

    Create Revisão Mundo 2 - Ex36.py

---
+++ Exercicios/Revisão Mundo 2 - Ex36.py
@@ -0,0 +1,10 @@
+#Ex: 36 -> Empréstimo bancário.
+valor = float(input('Digite o valor da casa: '))
+salário = float(input('Digite o salário: '))
+prazo = int(input('Digite o prazo para pagamento em anos: '))
+parcela = valor / (prazo * 12)
+if parcela > 30 / 100 * salário:
+    print(f'Valor da parcela R${parcela:.2f}. Empréstimo negado')
+elif parcela <= 30 / 100 * salário:
+    print(f'Valor da parcela R${parcela:.2f}. Empréstimo APROVADO!')
+print('Obrigaso por usar os nossos serviços')
