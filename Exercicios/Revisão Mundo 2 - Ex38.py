b006da86598f5fcc6e82df6d477d2ce727a75227
Joilton Rocha <joilton.rocha43@gmail.com>
2024-07-31 15:15:05 -0300

    Update Revisão Mundo 2 - Ex38.py

--- Exercicios/Revisão Mundo 2 - Ex38.py
+++ Exercicios/Revisão Mundo 2 - Ex38.py
@@ -2,18 +2,19 @@
 continuar = ''
 comp = igualdade = n1Mn2 = n1mn2 = 0
 while continuar in 'S':
+    print('-' * 35)
     num1 = int(input('Digite um número: '))
     num2 = int(input('Digite outro número: '))
     if num1 == num2:
-        print(f'{num1} é igual a {num2}.')
+        print(f'Os dois valores são iguais.')
         comp = comp + 1
         igualdade = igualdade + 1
     elif num1 < num2:
-        print(f'{num1} é menor que {num2}.')
+        print(f'O primeiro valor é menor.')
         comp = comp + 1
         n1mn2 = n1mn2 + 1
     else:
-        print(f'{num1} é maior que {num2}.')
+        print(f'O primeiro valor é maior.')
         comp = comp + 1
         n1Mn2 = n1Mn2 + 1
     continuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
@@ -22,8 +23,8 @@ while continuar in 'S':
 print('-' * 50)
 print(f'Você escolheu sair. Foram feitas {comp} comparações, das Quais:')
 if igualdade > 0:
-    print(f'* Os dois valores iguais: {igualdade}.')
+    print(f'* {igualdade}: Os dois valores iguais.')
 if n1mn2 > 0:
-    print(f'* O primeiro maior que o segundo: {n1mn2}.')
+    print(f'* {n1mn2}: O primeiro menor que o segundo.')
 if n1Mn2 > 0:
-    print(f'* O prdimeiro menor que o segundo: {n1Mn2}.')
+    print(f'* {n1Mn2}: O prdimeiro maior que o segundo.')
