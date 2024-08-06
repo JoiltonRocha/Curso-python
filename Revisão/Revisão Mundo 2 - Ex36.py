dd5f01e9663b7ce666bde2253a1e547b4ec31559
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-06 17:21:27 -0300

    Update Revisão Mundo 2 - Ex36.py

--- Revisão/Revisão Mundo 2 - Ex36.py
+++ Revisão/Revisão Mundo 2 - Ex36.py
@@ -1,10 +1,17 @@
 #Ex: 036 -> Empréstimo bancário.
-valor = float(input('Digite o valor da casa: '))
-salário = float(input('Digite o salário: '))
-prazo = int(input('Digite o prazo para pagamento em anos: '))
-parcela = valor / (prazo * 12)
-if parcela > 30 / 100 * salário:
-    print(f'Valor da parcela R${parcela:.2f}. Empréstimo negado')
-elif parcela <= 30 / 100 * salário:
-    print(f'Valor da parcela R${parcela:.2f}. Empréstimo APROVADO!')
-print('Obrigaso por usar os nossos serviços')
+while True:
+    valor = float(input('Digite o valor da casa: '))
+    salário = float(input('Digite o salário: '))
+    prazo = int(input('Digite o prazo para pagamento em anos: '))
+    parcela = valor / (prazo * 12)
+    if parcela > 30 / 100 * salário:
+        print(f'Valor da parcela R${parcela:.2f}. Empréstimo negado')
+    elif parcela <= 30 / 100 * salário:
+        print(f'Valor da parcela R${parcela:.2f}. Empréstimo APROVADO!')
+    print('Obrigaso por usar os nossos serviços')
+    continuar = str(input('Gostria de fazer uma nova simulação? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Gostria de fazer uma nova simulação? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
+print('FIM!!!')
