f714b90395d6a6be33fa38ee39f4d1142100a944
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-07 09:58:15 -0300

    Update Revisão Mundo 2 - Ex37.py

--- Revisão/Revisão Mundo 2 - Ex37.py
+++ Revisão/Revisão Mundo 2 - Ex37.py
@@ -1,25 +1,33 @@
 #Ex: 037 -> Base numérica.
-num = int(input('Digite um número: '))
-base = ('Binário', 'Octal', 'Hexadecimal')
-base = int(input('''Base de conversão: 
-- [1] Binário
-- [2] Octal
-- [3] Hexadecimal
-Opção: '''))
-if base == 1:
-    conversão = bin(num)[2:]
-    base = 'Binária'
-elif base == 2:
-    conversão = oct(num)[2:]
-    base = 'Octal'
-elif base == 3:
-    conversão = hex(num)[2:]
-    base = 'Hexadecimal'
-else:
-    while base not in '123':
-        base = int(input('''Base de conversão: 
+while True:
+    num = int(input('Digite um número: '))
+    base = ('Binário', 'Octal', 'Hexadecimal')
+    opção = str(input('''Base de conversão: 
+    - [1] Binário
+    - [2] Octal
+    - [3] Hexadecimal
+    Opção: '''))
+    while opção not in '123':
+        print('Opção inválida. Escolha uma das opções.')
+        opção = str(input('''Base de conversão: 
         - [1] Binário
         - [2] Octal
         - [3] Hexadecimal
         Opção: '''))
-print(f'O número {num} convertido para base {base} é {conversão}.')
+    if opção == '1':
+        conversão = bin(num)[2:]
+        base = 'BINÁRIO'
+    elif opção == '2':
+        conversão = oct(num)[2:]
+        base = 'OCTAL'
+    elif opção == '3':
+        conversão = hex(num)[2:]
+        base = 'HEXADECIMAL'
+    print(f'O número {num} convertido para {base} é {conversão}.')
+    continuar = str(input('Deseja continuar? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        print('Opção inválida.')
+        continuar = str(input('Deseja continuar? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
+print('FIM!!!')
