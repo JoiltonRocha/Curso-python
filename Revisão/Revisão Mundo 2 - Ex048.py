83c3a0c6fa4d62853900424ce530e61a2d9d229a
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-09 16:57:17 -0300

    Update Revisão Mundo 2 - Ex048.py

--- Revisão/Revisão Mundo 2 - Ex048.py
+++ Revisão/Revisão Mundo 2 - Ex048.py
@@ -1,5 +1,4 @@
 #Ex 048 -> Soma números ímpares.
-soma = cont = 0
 tipo = ['PARES E ÍMPARES', 'ÍMPARES', 'PARES']
 while True:
     inicio = int(input('Digite o número inicial: '))
@@ -10,20 +9,24 @@ while True:
     [1] APENAS ÍMPARES
     [2] APENAS PARES
     Sua opção -> '''))
-    if pi == 0:
-        for c in range (inicio, final, intervalo):
-            cont = cont + 1
-            soma = soma + c
-        print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final - 1} é {soma}.')
-    elif pi == 1 or pi == 2:
-        for c in range(inicio, final, intervalo):
-            if c % 2 == pi:
+    soma = cont = 0
+    for c in range(inicio, final, intervalo):
+        if pi == 0:
+            if pi % 2 == 1 or pi % 2 == 0:
                 cont = cont + 1
                 soma = soma + c
-        print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final} é {soma}.')
+        elif pi == 1:
+            if c % 2 == 1:
+                cont = cont + 1
+                soma = soma + c
+        elif pi == 2:
+            if c % 2 == 0:
+                cont = cont + 1
+                soma = soma + c
+    print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final - 1} é {soma}.')
     continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
     while continuar not in 'SN':
         continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
-    if continuar in 'N':
-        break
+        if continuar in 'N':
+            break
 print('FIM!!!')
