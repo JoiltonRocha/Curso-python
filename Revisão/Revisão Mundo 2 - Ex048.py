560e4fc7a13bb49ab48e85d147045681d798a92e
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-09 11:18:22 -0300

    Update Revisão Mundo 2 - Ex048.py

--- Revisão/Revisão Mundo 2 - Ex048.py
+++ Revisão/Revisão Mundo 2 - Ex048.py
@@ -1,6 +1,29 @@
 #Ex 048 -> Soma números ímpares.
-soma = 0
-for c in range(1, 500):
-    if c % 3 == 0 and c % 2 == 1:
-        soma = soma + c
-        print(soma)
+soma = cont = 0
+tipo = ['PARES E ÍMPARES', 'ÍMPARES', 'PARES']
+while True:
+    inicio = int(input('Digite o número inicial: '))
+    final = int(input('Digite o número final: '))
+    intervalo = int(input('Digite o intervalo: '))
+    pi = int(input('''*Escolha uma das opções de filtro paras os números desejados:
+    [0] PARES E ÍMPARES
+    [1] APENAS ÍMPARES
+    [2] APENAS PARES
+    Sua opção -> '''))
+    if pi == 0:
+        for c in range (inicio, final, intervalo):
+            cont = cont + 1
+            soma = soma + c
+        print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final} é {soma}.')
+    elif pi == 1 or pi == 2:
+        for c in range(inicio, final, intervalo):
+            if c % 2 == pi:
+                cont = cont + 1
+                soma = soma + c
+        print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final} é {soma}.')
+    continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
+print('FIM!!!')
