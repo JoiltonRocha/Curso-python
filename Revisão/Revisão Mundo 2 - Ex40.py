65091460257a60943dbaa9d469bbf3be5ab53b2c
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-09 09:51:14 -0300

    Update Revisão Mundo 2 - Ex40.py

--- Revisão/Revisão Mundo 2 - Ex40.py
+++ Revisão/Revisão Mundo 2 - Ex40.py
@@ -1,21 +1,22 @@
 #Ex 040 -> Média de notas.
-total = []
 contador = total = 0
 qnota = 1
+
 while True:
-    nota1 = float(input(f'Digite a {qnota}ª nota: '))
+    nota = float(input(f'Digite a {qnota}ª nota: '))
     qnota = qnota + 1
     contador = contador + 1
-    nota2 = float(input(f'Digite a {qnota}ª nota: '))
-    qnota = qnota + 1
-    contador = contador + 1
+    total = total + nota
     continuar = str(input('Deseja informar mais notas? [S/N] -> ')).upper().strip()[0]
     while continuar not in 'SN':
         ontinuar = str(input('Deseja informar mais notas? [S/N] -> ')).upper().strip()[0]
     if continuar in 'N':
         break
-    if contador == 2:
-        media = (qnota(1) + qnota(2)) / contador
-    if contador == 4:
-        media = (qnota(1) + qnota(2) + qnota(3) + qnota(4)) / contador
-print(media)
+media = total / contador
+if contador > 0:
+    if contador == 1:
+        print(f'Foi informada apenas 1 nota.')
+        print(f'MÉDIA -> {media}')
+    else:
+        print(f'Foram informadas {contador} notas.')
+        print(f'MÉDIA -> {media:.1f}')
