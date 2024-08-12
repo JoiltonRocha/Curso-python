b2687f4f7c653157b41226d57ad1d44061a57826
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 11:37:08 -0300

    Update Revisão Mundo 2 - Ex050.py

--- Revisão/Revisão Mundo 2 - Ex050.py
+++ Revisão/Revisão Mundo 2 - Ex050.py
@@ -3,17 +3,17 @@ tipo = ['PARES E ÍMPARES', 'ÍMPARES', 'PARES']
 while True:
     inicio = int(input('Digite o número inicial: '))
     final = int(input('Digite o número final: '))
-    intervalo = int(input('Digite o intervalo: '))
-    pi = int(input('''*Escolha uma das opções de filtro paras os números desejados:
+    pi = int(input('''*Escolha uma das opções de filtro paras os números informados a seguir:
     [0] PARES E ÍMPARES
     [1] APENAS ÍMPARES
     [2] APENAS PARES
     Sua opção -> '''))
     cont = soma = 0
-    for c in range(inicio, final, intervalo):
+    for c in range(inicio, final):
+        num = int(input(f'Digite o {c}º valor: '))
         if pi == 0:
-                cont = cont + 1
-                soma = soma + c
+            cont = cont + 1
+            soma = soma + c
         elif pi == 1:
             if c % 2 == 1:
                 cont = cont + 1
@@ -22,7 +22,7 @@ while True:
             if c % 2 == 0:
                 cont = cont + 1
                 soma = soma + c
-    print(f'A soma dos {cont} números {(tipo[pi])} entre {inicio} e {final - 1} é {soma}.')
+    print(f'A soma dos {cont} números {(tipo[pi])} informados é {soma}.')
     continuar = str(input('Deseja informar outros números? [S/N] -> '))
     if continuar in 'N':
         break
