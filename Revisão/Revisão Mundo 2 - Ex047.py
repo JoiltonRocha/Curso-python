ae320bc0fb6ebec7dd7b82ff26c203409b964ca2
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 11:01:27 -0300

    Update Revisão Mundo 2 - Ex047.py

--- Revisão/Revisão Mundo 2 - Ex047.py
+++ Revisão/Revisão Mundo 2 - Ex047.py
@@ -1,3 +1,31 @@
 #Ex 047 -> Contador números pares.
-for c in range(2, 51, 2):
-    print(c)
+tipo = ['PARES E ÍMPARES', 'ÍMPARES', 'PARES']
+while True:
+    inicial = int(input('Digite o número inicial: '))
+    final = int(input('Digite o número final: '))
+    intervalo = int(input('Digite o intervalo: '))
+    pi = int(input('''*Escolha uma das opções de filtro para os números desejados:
+    [0] PARES E ÍMPARES
+    [1] APENAS ÍMPARES
+    [2] APENAS PARES
+    Sua opção -> '''))
+    soma = cont = 0
+    for c in range(inicial, final, intervalo):
+        if pi == 0:
+            cont = cont + 1
+            soma = soma + c
+        elif pi == 1:
+            if c % 2 == 1:
+                cont = cont + 1
+                soma = soma + c
+        elif pi == 2:
+            if c % 2 == 0:
+                cont = cont + 1
+                soma = soma + c
+    print(f'A soma dos {cont} números {(tipo[pi])} entre {inicial} e {final - 1} é {soma}.')
+    continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Gostaria de informar outro número? [S/N] -> ')).upper().strip()[0]
+        if continuar in 'N':
+            break
+print('FIM!!!')
