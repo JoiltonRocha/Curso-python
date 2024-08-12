e92fdc33244f2fc9d9a9574c57e4ae8576312057
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 10:45:12 -0300

    Update Revisão Mundo 2 - Ex046.py

--- Revisão/Revisão Mundo 2 - Ex046.py
+++ Revisão/Revisão Mundo 2 - Ex046.py
@@ -1,6 +1,14 @@
-#Ex 046 -> Contador.
+#Ex 046 -> Contadorgem regressiva.
 from time import sleep
-for c in range (10, 0, -1):
-    print(c)
-    sleep(1)
-print('BUUUUUUUM!!!')
+while True:
+    inicial = int(input('Deseja iniciar a contagem regressiva de quanto: '))
+    for c in range (inicial, 0, -1):
+        print(c)
+        sleep(1)
+    print('BUUUUUUUM!!!')
+    continuar = str(input('Deseja fazer outra contagem? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Deseja fazer outra contagem? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N'
+        break
+print('FIM!!!')
