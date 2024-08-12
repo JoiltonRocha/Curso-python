cdcc5a4d9c53a56c416e8184ef5d1201df62124b
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 10:35:45 -0300

    Create Revisão Mundo 2 - Ex054.py

---
+++ Revisão/Revisão Mundo 2 - Ex054.py
@@ -0,0 +1,25 @@
+#Ex 054 -> Maioridade.
+from datetime import date
+somamais = somamin = soma = 0
+while True:
+    anoatual = date.today().year
+    anonasc = int(input('Digite o ano de nascimento: '))
+    idade = anoatual - anonasc
+    if idade >= 18:
+        somamais = somamais + 1
+    else:
+        somamin = somamin + 1
+    soma = soma + 1
+    continuar = str(input('Deseja informar outro ano de nascimento? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Deseja informar outro ano de nascimento? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
+print(f'Foram informados {soma} anos de nascimento. Dos quais:')
+if somamais > 0:
+    print(f'{somamais:2} -> Com idade maior ou igual a 18 anos.')
+if somamin > 0:
+    print(f'{somamin:2} -> Com idade menor que 18 anos.')
+
+
+
