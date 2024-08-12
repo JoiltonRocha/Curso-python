2c84fa2720b4f5feba1a498325009ca8217ce307
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 16:44:44 -0300

    Update Revisão Mundo 2 - Ex053.py

--- Revisão/Revisão Mundo 2 - Ex053.py
+++ Revisão/Revisão Mundo 2 - Ex053.py
@@ -1,1 +1,6 @@
 #Ex 053 -> Palíndromo.
+frase = str(input('Digite uma frase: ')).upper().strip()
+palavras = frase.strip()
+junto = join(palavras)
+inverso = ''
+print(f'Você digitou a frase {frase}: ')
