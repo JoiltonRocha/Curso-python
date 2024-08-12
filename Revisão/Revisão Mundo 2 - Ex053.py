4fa890955730144b9d90ea91b7e885ea709293df
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-12 17:19:02 -0300

    Update Revisão Mundo 2 - Ex053.py

--- Revisão/Revisão Mundo 2 - Ex053.py
+++ Revisão/Revisão Mundo 2 - Ex053.py
@@ -1,6 +1,18 @@
 #Ex 053 -> Palíndromo.
-frase = str(input('Digite uma frase: ')).upper().strip()
-palavras = frase.strip()
-junto = join(palavras)
-inverso = ''
-print(f'Você digitou a frase {frase}: ')
+while True:
+    frase = str(input('Digite uma frase: ')).strip().upper()
+    palavras = frase.split()
+    junto = ''.join(palavras)
+    inverso = ''
+    for c in range(len(junto) - 1, -1, -1):
+        inverso = inverso + junto[c]
+    print(f'O inverso de {junto} é {inverso}.')
+    if junto == inverso:
+        print('Temos um palíndromo!')
+    else:
+        print('A frase digitada não é um palíndromo!')
+    continuar = str(input('Deseja informar outra frase? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Deseja informar outra frase? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
