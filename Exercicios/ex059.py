44b3c100fd4d38435ddfefb7af6a2ed3d5239a57
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-13 16:39:23 -0300

    Update ex059.py

--- Exercicios/ex059.py
+++ Exercicios/ex059.py
@@ -7,9 +7,9 @@
 Seu programa deverá realizar a operação solicitada em cada caso.'''
 print(f'{'DESAFIO 59':=^45}')
 opção = 0
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
 while opção != 5:
-    n1 = int(input('Digite o primeiro valor: '))
-    n2 = int(input('Digite o segundo valor: '))
     print('''O que deseja fazer?
     [1] Somar.
     [2] Multiplicar.
@@ -33,13 +33,6 @@ while opção != 5:
     if opção == 4:
         n1 = int(input('Digite o primeiro valor: '))
         n2 = int(input('Digite o segundo valor: '))
-        print('''O que deseja fazer?
-        [1] Somar.
-        [2] Multiplicar.
-        [3] Maior.
-        [4] Novos números.
-        [5] Sair do programa.''')
-        opção = int(input('Opção: '))
     if opção == 5:
         print('Você escolheu sair.')
     if opção >= 6:
