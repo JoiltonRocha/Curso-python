9640fb62114074579df74518a2be7fb7db0ced76
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-13 09:01:29 -0300

    Update Revisão Mundo 2 - Ex055.py

--- Revisão/Revisão Mundo 2 - Ex055.py
+++ Revisão/Revisão Mundo 2 - Ex055.py
@@ -1,3 +1,21 @@
 #Ex 055-> Maior e menor.
+maior = 0
+menor = 0
 while True:
-    peso = float(input('Digite o peso: '))
+    for c in range(1, 6):
+        peso = float(input(f'Digite o peso da {c}ª pessoa: '))
+        if c == 1:
+            maior = peso
+            menor = peso
+        else:
+            if peso > maior:
+                maior = peso
+            if peso < menor:
+                menor = peso
+    print(f'O maior peso lido foi {maior}Kg.')
+    print(f'O menor peso lido foi {menor}Kg.')
+    continuar = str(input('Deseja continuar? [S/N] -> ')).upper().strip()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Deseja informar continuar? [S/N] -> ')).upper().strip()[0]
+    if continuar in 'N':
+        break
