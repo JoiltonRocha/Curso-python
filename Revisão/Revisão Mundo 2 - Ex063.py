5c76edad8c71359a9c0418046696cc7d6eeece7c
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-14 16:54:09 -0300

    Create Revisão Mundo 2 - Ex063.py

---
+++ Revisão/Revisão Mundo 2 - Ex063.py
@@ -0,0 +1,13 @@
+#Ex 063 -> Sequência de Fibonacci.
+num = int(input('Informe quantos termos da Sequência de Fibonacci deseja mostrar: '))
+termo1 = 0
+termo2 = 1
+print(f'{termo1} -> {termo2} -> ', end='')
+cont = 3
+while cont <= num:
+    termo3 = termo1 + termo2
+    print(f'{termo3} -> ', end='')
+    termo1 = termo2
+    termo2 = termo3
+    cont = cont + 1
+print('FIM!!!')
