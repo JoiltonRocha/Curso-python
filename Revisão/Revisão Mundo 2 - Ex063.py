6a7391897827a7dfc46793d7298a84ecfb2b5294
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-15 10:09:29 -0300

    Update Revisão Mundo 2 - Ex063.py

--- Revisão/Revisão Mundo 2 - Ex063.py
+++ Revisão/Revisão Mundo 2 - Ex063.py
@@ -1,13 +1,19 @@
 #Ex 063 -> Sequência de Fibonacci.
-num = int(input('Informe quantos elementos da Sequêcia de Fibonacci deseja mostrar: '))
-termo1 = 0
-termo2 = 1
-print(f'{termo1} -> {termo2} -> ', end='')
+num = int(input('Informe quantos elementos da Sequência de Fibonacci deseja mostrar: '))
+elemento1 = 0
+elemento2 = 1
+print(f'{elemento1} -> {elemento2} -> ', end='')
 cont = 3
-while cont <= num:
-    termo3 = termo1 + termo2
-    print(f'{termo3} -> ', end='')
-    termo1 = termo2
-    termo2 = termo3
-    cont = cont + 1
-print('FIM!')
+mais = num
+total = 0
+while mais > 0:
+    total = total + mais
+    while cont <= total:
+        elemento3 = elemento1 + elemento2
+        print(f'{elemento3} -> ', end='')
+        elemento1 = elemento2
+        elemento2 = elemento3
+        cont = cont + 1
+    print('PAUSA')
+    mais = int(input('Informe quantos elementos mais deseja mostrar ou, tecle [0] para sair: '))
+print(f'FIM! Foram mostrados {total} elementos.')
