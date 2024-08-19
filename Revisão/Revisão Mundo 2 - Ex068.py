9e64096b57f66c338d3e97e43b267941bfb17d26
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-19 18:41:53 -0300

    Create Revisão Mundo 2 - Ex068.py

---
+++ Revisão/Revisão Mundo 2 - Ex068.py
@@ -0,0 +1,33 @@
+#Ex 068 -> Jogo do Par ou Ímpar.
+from random import randint
+print('VAMOS JOGAR PAR OU ÍMPAR')
+v = 0
+while True:
+    computador = randint(1, 10)
+    jogador = int(input('Digite o seu número: '))
+    total = computador + jogador
+    tipo = str(input('Par ou Ímpar [P/I] -> ')).upper().strip()[0]
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar [P/I] -> ')).upper().strip()[0]
+    print(f'Você jogou {jogador} e o computador {computador}, totalizando {total}, ', end='')
+    print('QUE É PAR.' if total % 2 == 0 else 'QUE É ÍMPAR.')
+    if tipo in 'P':
+        if total % 2 == 0:
+            v = v + 1
+            print('Você VENCEU! Pode jogar novamente.')
+        else:
+            break
+    elif tipo in 'I':
+        if total % 2 != 0:
+            v = v + 1
+            print('Você VENCEU! Pode jogar novamente.')
+        else:
+            break
+    print('+-' * 30)
+if v == 0:
+    print(f'GAME OVER! Sem vencer uma vez, sequer.')
+else:
+    if v == 1:
+        print(f'GAME OVER! Você venveu {v} apenas uma vez.')
+    elif v > 1:
+        print(f'GAME OVER! Você venceu {v} vezes consecutivas.')
