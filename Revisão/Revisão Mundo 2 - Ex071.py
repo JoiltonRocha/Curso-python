7d4576d8708ef593a32f6511f804a24cd8a4cfb2
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-21 08:40:52 -0300

    Update Revisão Mundo 2 - Ex071.py

--- Revisão/Revisão Mundo 2 - Ex071.py
+++ Revisão/Revisão Mundo 2 - Ex071.py
@@ -1,7 +1,7 @@
 #Ex 071 -> Simulador de caixa eletrônico.
 saque = int(input('Informe o valor do saque: R$'))
 total = saque
-céd = 50
+céd = 200
 totcéd = 0
 while True:
     if total >= céd:
@@ -10,11 +10,17 @@ while True:
     else:
         if totcéd > 0:
             print(f'Total de {totcéd} cédulas de R${céd:.2f}')
-        if céd == 50:
+        if céd == 200:
+            céd = 100
+        elif céd == 100:
+            céd = 50
+        elif céd == 50:
             céd = 20
         elif céd == 20:
             céd = 10
         elif céd == 10:
+            céd = 5
+        elif céd == 5:
             céd = 1
         totcéd = 0
         if total == 0:
