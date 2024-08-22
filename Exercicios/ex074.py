a8c2ccc9d4c001cf5f95bb72ae387b9c0fac37a3
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-22 19:37:21 -0300

    Update ex074.py

--- Exercicios/ex074.py
+++ Exercicios/ex074.py
@@ -2,8 +2,20 @@
 Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.'''
 print(f'{'DESAFIO 74':=^45}')
 from random import randint
-cont = 0
+cont = maior = menor = 0
+print('Os valores sorteados foram: ', end='')
 while cont < 5:
-    num = randint()
+    num = randint(0, 10)
     cont = cont + 1
-print(num)
+    tupla = (num)
+    print(f'{tupla} ', end='')
+    if cont == 1:
+        menor = num
+        maior = num
+    else:
+        if num < menor:
+            menor = num
+        if num > maior:
+            maior = num
+print(f'\nO maior número sorteado foi {maior}')
+print(f'O menor número sorteado foi {menor}')
