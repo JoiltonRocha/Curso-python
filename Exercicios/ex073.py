fbb08a6e6f32671af15f32f28e9f5f94d441d49f
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-23 09:11:52 -0300

    Update ex073.py

--- Exercicios/ex073.py
+++ Exercicios/ex073.py
@@ -5,14 +5,19 @@ b) Os últimos colocados da tabela.
 c) Uma lista com os times em ordem afabética.
 d) Em que posição na tabela está o time da Chapecoense.'''
 print(f'{'DESAFIO 73':=^45}')
-tupla = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport Recife', 'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-GO')
+tupla = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense',
+         'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta',
+         'Atlético-GO')
 print('*' * 50)
 print(f'-> Lista de times do Brasileirão: {tupla}')
 print('*' * 50)
-print(f'-> Os cinco primeiros são: {tupla[0:6]}')
+print(f'-> Os cinco primeiros são: {tupla[0:5]}')
 print('*' * 50)
-print(f'-> Os quatro últimos são: {tupla[16:]}')
+print(f'-> Os quatro últimos são: {tupla[-4:]}')
 print('*' * 50)
 print(f'-> Times em ordem alfabética: {sorted(tupla)}')
 print('*' * 50)
-print(f'-> O Chapecoense está na {tupla.index('Chapecoense')}ª posição.')
+print(f'-> O Chapecoense está na {tupla.index('Chapecoense')+1}ª posição.')
