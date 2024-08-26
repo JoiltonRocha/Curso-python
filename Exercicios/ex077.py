f668f9940bbc769d9002608bb415863e833e2f0d
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-26 10:23:19 -0300

    Create ex077.py

---
+++ Exercicios/ex077.py
@@ -0,0 +1,7 @@
+'''Crie um program que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
+para cada palavra, quais são vogais.'''
+print(f'{'DESAFIO 77':=^45}')
+tupla = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON',
+         'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR',
+         'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+if tupla in 'AEIOU':
