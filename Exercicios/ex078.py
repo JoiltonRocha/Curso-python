d49c88efc42f110699ab234a0e246e5f0f421b5d
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-27 08:58:31 -0300

    Update ex078.py

--- Exercicios/ex078.py
+++ Exercicios/ex078.py
@@ -1,5 +1,6 @@
 '''Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.'''
+print(f'{'DESAFIO 78':=^45}')
 valores = list()
 maior = menor = cont = posmaior = posmenor = 0
 for c in range(0, 5):
@@ -7,18 +8,18 @@ for c in range(0, 5):
     if len(valores) == 1:
         maior = valores[0]
         menor = valores[0]
-        print(enumerate(valores))
-        #posmaior = enumerate(valores)
-        #posmenor = enumerate(valores)
+        posmaior = enumerate(valores[cont])
+        posmenor = enumerate(valores[cont])
     else:
         cont = cont + 1
         if valores[cont] > maior:
             maior = valores[cont]
-            posmaior = enumerate(valores)
+            posmaior = valores[cont]
         if valores[cont] < menor:
             menor = valores[cont]
-            posmenor = enumerate(valores)
+            posmenor = valores[cont]
+for pos, v in enumerate(valores):
+    print('Fim')
 print(f'Você digitou os valores {valores}')
-print(f'O menor valor digitado foi {menor} na posição {posmenor}.')
-print(f'O maior valor digitado foi {maior} na posição {posmaior}.')
-
+print(f'O menor valor digitado foi {menor} na posição {pos}.')
+print(f'O maior valor digitado foi {maior} na posição {pos}.')
