b034c434ca41876070a82cf450a76b0d45442866
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-27 16:24:43 -0300

    Update ex080.py

--- Exercicios/ex080.py
+++ Exercicios/ex080.py
@@ -4,5 +4,17 @@ No final, mostre a lista ordenada na tela.'''
 print(f'{'DESAFIO 80':=^45}')
 num = list()
 for c in range(0, 5):
-    num.append(int(input('Digite um número: ')))
-    
+    n = (int(input('Digite um número: ')))
+    if c == 0 or n > num[-1]:
+        num.append(n)
+        print(f'"{n}" adicionado no final da lista.')
+    else:
+        pos = 0
+        while pos < len(num):
+            if n <= num[pos]:
+                num.insert(pos, n)
+                print(f'"{n}" adicionado na posição {pos} da lista')
+                break
+            pos = pos + 1
+print('=' * 30)
+print(f'Os valores digitados, ordenados em ordem crescente fica assim: {num}')
