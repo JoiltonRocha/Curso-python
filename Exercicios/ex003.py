bbecd33797f93fe41951d3a824d07981158f75f9
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-29 11:53:31 -0300

    Update ex003.py

--- Exercicios/ex003.py
+++ Exercicios/ex003.py
@@ -1,4 +1,5 @@
 print('====== DESAFIO 03======')
-n1 = input('Primeiro número: ')
-n2 = input('Segundo número: ')
-print('A soma é ', int(n1) + int(n2))
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é {s}.')
