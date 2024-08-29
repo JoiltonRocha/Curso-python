7bfadb7ccf175ed641c177e29f45d23db24b8fe4
Joilton Rocha <joilton.rocha43@gmail.com>
2024-08-29 11:45:53 -0300

    Update ex003.py

--- Exercicios/ex003.py
+++ Exercicios/ex003.py
@@ -1,4 +1,4 @@
 print('====== DESAFIO 03======')
-n1 = input('Primeiro número ')
-n2 = input('Segundo número ')
+n1 = input('Primeiro número: ')
+n2 = input('Segundo número: ')
 print('A soma é ', int(n1) + int(n2))
